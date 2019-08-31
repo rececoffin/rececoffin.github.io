0edf538ca80583e7b85d25c6fc450d8763c26a47
Rece Coffin <rcoffin@mymail.mines.edu>
2019-08-31 12:51:30 -0600

    More changes

---
+++ precache-manifest.dd1b8eed606c4af97e6b94d5c7fc39a3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f720b6ec609fee115294db28a437ab3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e1369caece9022840c1",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "df91d0a9972900339d3e",
+    "url": "/static/js/2.74c7c01c.chunk.js"
+  },
+  {
+    "revision": "1e1369caece9022840c1",
+    "url": "/static/js/main.cd3e5eaa.chunk.js"
+  },
+  {
+    "revision": "42ff2209f6f71e8c2f9c",
+    "url": "/static/js/runtime~main.095aed60.js"
+  }
+]);
